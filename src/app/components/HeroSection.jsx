b42c8b101ed4da013d7038f56780277b65bc22e5
Giventus <weishen735@gmail.com>
2024-11-24 16:53:51 +0700

    init

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,13 @@
+export default function HeroSection() {
+  return (
+    <section>
+      <h1 className="text-white mb-4 text-6xl font-extrabold">
+        Hello, I'm Giventus
+      </h1>
+      <p className="text-[#ADB7BE]">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+        deserunt minima ipsa laborum explicabo necessitatibus quis..
+      </p>
+    </section>
+  );
+}
