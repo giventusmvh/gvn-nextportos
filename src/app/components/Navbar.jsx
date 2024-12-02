00610978da6fd2cd9ee330e049af70d6ac5f13a9
Giventus <weishen735@gmail.com>
2024-12-02 20:15:47 +0700

    ok

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -16,11 +16,11 @@ const navLink = [
 export default function Navbar() {
   const [navbarOpen, setNavbarOpen] = useState(false);
   return (
-    <nav className="fixed top-0 left-0 right-0 z-20 bg-[#121212]/30 backdrop-blur-md  border-b border-white">
-      <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-3 md:max-w-[1400px]">
-        <Link
+    <nav className="mx-auto fixed top-5 left-0 right-0 z-20 bg-[#121212]/30 backdrop-blur-md  border border-white md:max-w-[1400px] rounded-full max-w-[95%]">
+      <div className="flex flex-wrap items-center justify-center mx-auto px-4 py-3 md:max-w-[1400px]">
+        {/* <Link
           href={"/"}
-          className="text-4xl text-transparent bg-clip-text  bg-gradient-to-r from-primary-600 to-secondary-400 hover:bg-slate-200 font-semibold font-sans"
+          className="text-4xl text-transparent bg-clip-text  bg-gradient-to-r from-primary-600 to-secondary-400 hover:bg-slate-200 font-normal font-sans"
         >
           <Image
             src={"/images/sign-white2.png"}
@@ -28,9 +28,9 @@ export default function Navbar() {
             width={140}
             height={50}
           />
-          {/* {"GM"} */}
-        </Link>
-        <div className="mobile-menu block md:hidden">
+          {"GiventusMarco"}
+        </Link> */}
+        {/* <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
             <button
               onClick={() => setNavbarOpen(true)}
@@ -46,9 +46,9 @@ export default function Navbar() {
               <XMarkIcon className="w-5 h-5" />
             </button>
           )}
-        </div>
-        <div className="menu hidden md:block md:w-auto" id="navbar">
-          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-12 mt-0">
+        </div> */}
+        <div className="menu block md:w-auto" id="navbar">
+          <ul className="flex p-0 md:p-0 md:flex-row md:space-x-14 mt-0">
             {navLink.map((link, index) => (
               <li key={index}>
                 <Navlink href={link.href} title={link.title} />
